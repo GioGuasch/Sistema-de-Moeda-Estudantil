04c148237f33c67dd43a370ae28b0554ae0dfd16
Natalie Abreu <84210490+natalie313@users.noreply.github.com>
2025-11-16 23:20:07 -0300

    Delete 03. Codigos directory

--- 03. Codigos/backend/src/routes/index.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import vantagemRoutes from './vantagemRoutes.js';
-const router = Router();
-router.use('/vantagens', vantagemRoutes);
-export default router;
--- 03. Codigos/frontend/js/esqueceuSenha.js
+++
@@ -1,6 +0,0 @@
-function enviarEmail(event) {
-    event.preventDefault();
-    alert("Email enviado!");
-    window.location.href = "login.html";
-    return false;
-}
