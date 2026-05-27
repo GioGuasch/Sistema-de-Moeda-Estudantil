d62619d7f65eda0738534124b9c32273fe7ff261
Giovanna Guasch <133532207+GioGuasch@users.noreply.github.com>
2026-05-27 18:11:55 -0300

    Refactor consultarSaldo.php for improved readability

--- 03. Codigos/back/consultarSaldo.php
+++ 03. Codigos/back/consultarSaldo.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 include("conexao.php");
-
+ 
 $codigoAluno = $_POST["codigoAluno"] ?? null;
 
 if (!$codigoAluno) {
