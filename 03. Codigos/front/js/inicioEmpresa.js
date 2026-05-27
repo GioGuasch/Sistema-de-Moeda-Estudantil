56d943ba7b0d7ceb85202e95633878e8266bc8e4
Giovanna Guasch <133532207+GioGuasch@users.noreply.github.com>
2026-05-27 18:16:46 -0300

    Update inicioEmpresa.js

--- 03. Codigos/front/js/inicioEmpresa.js
+++ 03. Codigos/front/js/inicioEmpresa.js
@@ -5,7 +5,7 @@ userIcon.addEventListener("click", () => {
     userDropdown.style.display = 
         userDropdown.style.display === "block" ? "none" : "block";
 });
-
+ 
 document.addEventListener("click", (e) => {
     if (!userIcon.contains(e.target) && !userDropdown.contains(e.target)) {
         userDropdown.style.display = "none";
