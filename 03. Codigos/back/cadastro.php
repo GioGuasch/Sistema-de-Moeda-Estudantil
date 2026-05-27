e425cde98770fad529fe3bb4c594f2c06b59aa8f
Giovanna Guasch <133532207+GioGuasch@users.noreply.github.com>
2026-05-27 15:58:37 -0300

    Update cadastro.php

--- 03. Codigos/back/cadastro.php
+++ 03. Codigos/back/cadastro.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
 include("conexao.php");
-
+ 
 $dados = $_POST;
 
 $tipo = $dados["tipoUsuario"] ?? '';
